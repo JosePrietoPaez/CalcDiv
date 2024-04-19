0089273597608b2216ab2908bfd41f3bd50d7396
JosePrietoPaez <0610904705@uma.es>
2024-04-19 17:49:28 +0200

    Creado el proyecto de pruebas
    Para un trabajo de la uni crearé pruebas en grupo para la aplicación.
    Increíble que por esto me diera cuenta de que subí el 0.1 de expanded lists aquí

---
+++ TestCalculadora/CalculadoraDivisibilidadTest.cs
@@ -0,0 +1,12 @@
+namespace TestCalculadora {
+	public class CalculadoraDivisibilidadTest {
+		[SetUp]
+		public void Setup() {
+		}
+
+		[Test]
+		public void Test1() {
+			Assert.Pass();
+		}
+	}
+}	
